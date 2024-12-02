5d36c9425a12b4bcf81dae638e3b57ccbed7f30e
Gustavo Silva <gustacar2008@gmail.com>
2024-12-02 13:04:36 -0300

    Enhance UI with hover effects for navigation and product sections

--- src/components/products/sessaoProducts.tsx
+++ src/components/products/sessaoProducts.tsx
@@ -7,7 +7,7 @@ export default function SessaoProducts() {
             <h1 className="text-2xl">Products</h1>
             <div className="flex flex-col items-center w-[100%] gap-3">
                 <Products />
-                <div className="flex gap-2">
+                <div className="flex gap-2 relative group">
                     <a href="">More</a>
                     <Image
                         src='/arrow.svg'
@@ -15,6 +15,7 @@ export default function SessaoProducts() {
                         width={10}
                         height={10}
                     />
+                    <div className="absolute bottom-0 left-0 w-0 h-[2px] bg-white transition-all duration-300 group-hover:w-full"></div>
                 </div>
             </div>
         </div>
